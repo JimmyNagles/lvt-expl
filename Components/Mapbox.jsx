134f94fb680e1c2e07edc9c5c2ff8a807f38e3e1
jimmynagles <jimmynagles@gmail.com>
2023-11-05 14:40:26 -0800

    css and property[id] added

--- Components/Mapbox.jsx
+++ Components/Mapbox.jsx
@@ -1,18 +1,17 @@
 import React, { useEffect, useRef, useState } from "react";
 import mapboxgl from "mapbox-gl";
-import "mapbox-gl/dist/mapbox-gl.css"; // Import the CSS
+import "mapbox-gl/dist/mapbox-gl.css";
 
 mapboxgl.accessToken =
-  "pk.eyJ1IjoiamltbXk5NiIsImEiOiJja3k1NTd6cjYwaWd2Mm5wdm9ydndtbmY2In0.I8ineDR8DIBU-Igc6eUVig"; // Your Mapbox access token
+  "pk.eyJ1IjoiamltbXk5NiIsImEiOiJja3k1NTd6cjYwaWd2Mm5wdm9ydndtbmY2In0.I8ineDR8DIBU-Igc6eUVig";
 
 const Mapbox = () => {
   const mapContainerRef = useRef(null);
-  const hoveredFeatureId = useRef(null); // Use useRef here
+  const hoveredFeatureId = useRef(null);
   const [geojsonData, setGeojsonData] = useState(null);
 
   const classifyData = (data) => {
     data.features.forEach((feature) => {
-      // Example classification: Add a 'color' property based on 'zoning_code'
       feature.properties.color = getColorByZoningCode(
         feature.properties.zoning_code
       );
@@ -23,18 +22,19 @@ const Mapbox = () => {
   const getColorByZoningCode = (zoningCode) => {
     switch (zoningCode) {
       case "P":
-        return "#00FF00"; // Green for Public
+        return "#00FF00";
       case "RH-1":
       case "RH-1(D)":
       case "RH-1(S)":
-        return "#0000FF"; // Blue for Residential - House
+        return "#0000FF";
       case "RM-1":
       case "RM-2":
         return "#FFFF00";
       default:
-        return "#888888"; // Default color
+        return "#888888";
     }
   };
+
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: mapContainerRef.current,
@@ -47,20 +47,15 @@ const Mapbox = () => {
       fetch("/all.geojson")
         .then((response) => response.json())
         .then((data) => {
-          // Ensure each feature has a unique `id`
           const classifiedData = classifyData(data);
           setGeojsonData(classifiedData);
 
-          console.log(data);
-          console.log(geojsonData);
-
           data.features.forEach((feature, index) => {
             if (!feature.id) {
-              feature.id = index.toString(); // Assigning a unique ID to each feature if it doesn't have one
+              feature.id = index.toString();
             }
           });
 
-          // Add the processed data as a source to the map
           map.addSource("boundaries", {
             type: "geojson",
             data: data,
@@ -74,8 +69,8 @@ const Mapbox = () => {
               "fill-color": [
                 "case",
                 ["boolean", ["feature-state", "hover"], false],
-                "#FF0000", // Color when hovered
-                ["get", "color"], // Use the 'color' property from the feature's properties object
+                "#FF0000",
+                ["get", "color"],
               ],
               "fill-opacity": 0.4,
             },
@@ -86,6 +81,8 @@ const Mapbox = () => {
             closeButton: false,
             closeOnClick: false,
           });
+
+          let clickedFeatureId = null;
 
           map.on("mouseenter", "plot-boundary", (e) => {
             map.getCanvas().style.cursor = "pointer";
@@ -103,16 +100,82 @@ const Mapbox = () => {
 
             const coordinates = e.lngLat;
             const properties = e.features[0].properties;
+            console.log(properties);
 
-            // Extract and format the desired properties
             const description = `
-    <strong>Plot Name:</strong> ${properties.name || "N/A"}<br>
-    <strong>Zoning Code:</strong> ${properties.zoning_code || "N/A"}<br>
-    <strong>Zoning District:</strong> ${properties.zoning_district || "N/A"}<br>
-    <strong>Block Number:</strong> ${properties.block_num || "N/A"}<br>
-    <strong>Lot Number:</strong> ${properties.lot_num || "N/A"}
-`;
+              <div class="p-2 bg-black text-white">
+                <strong class="p-2 mt-2">Plot Name:</strong> ${
+                  properties.name || "N/A"
+                }<br>
+                <strong class="p-2 mt-2">Zoning Code:</strong> ${
+                  properties.zoning_code || "N/A"
+                }<br>
+                <strong class="p-2 mt-2">Zoning District:</strong> ${
+                  properties.zoning_district || "N/A"
+                }<br>
+                <strong class="p-2 mt-2">Block Number:</strong> ${
+                  properties.block_num || "N/A"
+                }<br>
+                <strong class="p-2 mt-2">Lot Number:</strong> ${
+                  properties.lot_num || "N/A"
+                }<br>
+                <strong class="p-2 mt-2">property value:</strong> ${
+                  properties.property_value || "N/A"
+                }
 
+    <button class="p-2 mt-4 text-blue-400" onclick="window.open('/property/${
+      properties.id
+    }', '_blank')">
+    go to page</button> 
+              </div>
+            `;
+
+            popup.setLngLat(coordinates).setHTML(description).addTo(map);
+          });
+
+          map.on("click", "plot-boundary", (e) => {
+            if (clickedFeatureId !== null) {
+              map.setFeatureState(
+                { source: "boundaries", id: clickedFeatureId },
+                { hover: false }
+              );
+            }
+
+            clickedFeatureId = e.features[0].id;
+            map.setFeatureState(
+              { source: "boundaries", id: clickedFeatureId },
+              { hover: true }
+            );
+
+            const coordinates = e.lngLat;
+            const properties = e.features[0].properties;
+
+            const description = `
+            <div class="p-2 bg-black text-white">
+    <strong class="p-2 mt-2">Plot Name:</strong> ${properties.name || "N/A"}<br>
+    <strong class="p-2 mt-2">Zoning Code:</strong> ${
+      properties.zoning_code || "N/A"
+    }<br>
+    <strong class="p-2 mt-2">Zoning District:</strong> ${
+      properties.zoning_district || "N/A"
+    }<br>
+    <strong class="p-2 mt-2">Block Number:</strong> ${
+      properties.block_num || "N/A"
+    }<br>
+    <strong class="p-2 mt-2">Lot Number:</strong> ${
+      properties.lot_num || "N/A"
+    }<br>
+    <strong class="p-2 mt-2">property value:</strong> ${
+      properties.property_value || "N/A"
+    }<br>
+
+    <button class="p-2 mt-4 text-blue-400" onclick="window.open('/property/${
+      properties.id
+    }', '_blank')">
+ go to page</button> 
+    <br>
+      </div>
+`;
             popup.setLngLat(coordinates).setHTML(description).addTo(map);
           });
 
@@ -125,7 +188,21 @@ const Mapbox = () => {
               );
               hoveredFeatureId.current = null;
             }
-            popup.remove();
+          });
+
+          map.on("click", (e) => {
+            const features = map.queryRenderedFeatures(e.point, {
+              layers: ["plot-boundary"],
+            });
+
+            if (!features.length && clickedFeatureId !== null) {
+              map.setFeatureState(
+                { source: "boundaries", id: clickedFeatureId },
+                { hover: false }
+              );
+              clickedFeatureId = null;
+              popup.remove();
+            }
           });
         })
         .catch((error) => {
