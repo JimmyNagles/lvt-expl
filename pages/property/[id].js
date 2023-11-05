3836e3ad5670acd2a4f169f39070b574cf44ca35
jimmynagles <jimmynagles@gmail.com>
2023-11-05 15:47:19 -0800

    propery dashboard css

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -1,38 +1,77 @@
 import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 
 const Property = () => {
   const router = useRouter();
   const { id } = router.query;
 
-  // For now, let's use mock data. In a real-world scenario, you'd fetch the property details using this ID.
-  const property = {
-    name: "Sample Property",
-    zoning_code: "RH-1",
-    zoning_district: "Residential - House",
-    block_num: "12345",
-    lot_num: "67890",
-    property_value: "$500,000",
-  };
+  const [property, setProperty] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (id) {
+      fetch("/all.geojson")
+        .then((response) => response.json())
+        .then((data) => {
+          const propertyDetails = data.features.find(
+            (feature) => feature.properties.blklot === id
+          );
+          setProperty(propertyDetails);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.error("Error fetching property details:", error);
+          setLoading(false);
+        });
+    }
+  }, [id]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!property) {
+    return <div>Property not found.</div>;
+  }
 
   return (
-    <div className="p-2 mt-2">
+    <div className="p-20">
       <button onClick={() => router.back()}>Back to Map</button>
-      <h1>{property.name}</h1>
-      <p>
-        <strong>Zoning Code:</strong> {property.zoning_code}
-      </p>
-      <p>
-        <strong>Zoning District:</strong> {property.zoning_district}
-      </p>
-      <p>
-        <strong>Block Number:</strong> {property.block_num}
-      </p>
-      <p>
-        <strong>Lot Number:</strong> {property.lot_num}
-      </p>
-      <p>
-        <strong>Property Value:</strong> {property.property_value}
-      </p>
+
+      <div className="grid grid-cols-3 gap-4 ">
+        <div className="p-2 bg-gray-300 mt-2 mb-2 shadow-2xl flex flex-col justify-center w-[200px] h-[100px]">
+          <p>
+            <strong>Zoning Code:</strong> {property.properties.zoning_code}
+          </p>
+        </div>
+
+        <div className="p-2 bg-gray-300 mt-2 mb-2 shadow-2xl flex flex-col justify-center w-[200px] h-[100px]">
+          <p>
+            <h1>{property.properties.street_name}</h1>
+          </p>
+        </div>
+
+        <div className="p-2 bg-gray-300 mt-2 mb-2 shadow-2xl flex flex-col justify-center w-[200px] h-[100px]">
+          <p>
+            <strong>Zoning District:</strong>{" "}
+            {property.properties.zoning_district}
+          </p>
+        </div>
+
+        <div className="p-2 bg-gray-300 mt-2 mb-2 shadow-2xl flex flex-col justify-center w-[200px] h-[100px]">
+          <p>
+            <strong>Block Number:</strong> {property.properties.block_num}
+          </p>
+        </div>
+
+        <div className="p-2 bg-gray-300 mt-2 mb-2 shadow-2xl flex flex-col justify-center w-[200px] h-[100px]">
+          <p>
+            <strong>Lot Number:</strong> {property.properties.lot_num}
+          </p>
+        </div>
+      </div>
+
+      {/* ... Add other properties here ... */}
     </div>
   );
 };
